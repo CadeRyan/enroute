c2ccbc7765ea53f8dee73416774c36e49a2eaf66
Cade Ryan <caryan@microsoft.com>
2019-05-07 11:42:27 +0100

    adding strongly typed JSON querying

--- app/src/main/java/com/transport/enroute/RealtimeQuery.java
+++ app/src/main/java/com/transport/enroute/RealtimeQuery.java
@@ -3,12 +3,19 @@ package com.transport.enroute;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URL;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -24,17 +31,19 @@ public class RealtimeQuery {
                 try{
                     searchBar.setText(stopNumber);
 
-                    URL url = new URL("https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=" + stopNumber + "&format=xml");
-                    NodeList results = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(url.openStream()).getElementsByTagName("result");
+                    URL url = new URL("https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation?stopid=" + stopNumber + "&format=json");
+
+                    Gson gson = new Gson();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+                    StopRealTime stopResult = gson.fromJson(br, StopRealTime.class);
 
                     String timesStr = "";
                     String busesStr = "";
 
-                    for (int i = 0; i < results.getLength(); i ++){
+                    for (Result res : stopResult.getResults()){
 
-                        Element tmp = (Element) results.item(i);
-                        busesStr += tmp.getElementsByTagName("route").item(0).getTextContent() + "\n";
-                        String dueTime = tmp.getElementsByTagName("duetime").item(0).getTextContent();
+                        busesStr += res.getRoute() + "\n";
+                        String dueTime = res.getDuetime();
                         switch (dueTime){
                             case("Due"):{
                                 timesStr += dueTime + "\n";
@@ -50,16 +59,10 @@ public class RealtimeQuery {
                         }
                     }
 
-                    if (busesStr.equals("")) {
-                        busesStr += "No results";
-                    }
-
                     route.setText(busesStr);
                     times.setText(timesStr);
 
-                }catch (ParserConfigurationException | IOException e) {
-                    e.printStackTrace();
-                }catch (SAXException e) {
+                }catch (IOException e) {
                     e.printStackTrace();
                 }
             }
